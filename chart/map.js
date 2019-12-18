36b75006e1b5a267778d1ac87501dfb6099016c0
Cesar <cemoga@yahoo.com>
2019-12-18 06:09:21 -0500

    Plot Ready

--- chart/map.js
+++ chart/map.js
@@ -3,7 +3,8 @@
 function thousands_separators(num) {
   var num_parts = num.toString().split(".");
   num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  return num_parts.join(".");
+  // return num_parts.join(".");
+  return num_parts[0];
 }
 
 
@@ -21,6 +22,9 @@ Plotly.d3.csv('../output_charts/full_query.csv', function (err, rows) {
     schoolLong = unpack(rows, 'location_lon'),
     schoolState = unpack(rows, 'state_fips_dec'),
     schoolCity = unpack(rows, 'city'),
+    facultySalary = unpack(rows, 'faculty_salary'),
+    expenditurePerStudent = unpack(rows, 'instructional_expenditure_per_fte'),
+    tuitionRevenuePerStudent = unpack(rows, 'tuition_revenue_per_fte'),
     listofStates = [], // Stores all the State names
     citySizeInState = [], // 1. Size of Marker "In-State Tuition"
     citySizeOutState = [], // 2. Size of Marker "Out-of-State Tuition"
@@ -31,7 +35,14 @@ Plotly.d3.csv('../output_charts/full_query.csv', function (err, rows) {
     listLat = [], // 7. Longitude
     listLong = [], // 8. Latitude
     citySize = [], // 9. Variable to be use depending the user selection
-    scale = 3000;
+    expPerStudent = [], // 10. Variable to bring the expenditure per student
+    facSalary = [], // 11. Faculty Salary
+    tuRevPerStudent = [], // 12. Tuition Revenue per Student
+    scaleTuition = 3000, // 
+    scaleExp = 3000,
+    scaleFac = 1000,
+    scaleRev = 3000,
+    scaleVar = 0; // Used to store the variable scales
 
   // Loop for getting all State Names
   for (var i = 0; i < schoolState.length; i++) {
@@ -40,8 +51,8 @@ Plotly.d3.csv('../output_charts/full_query.csv', function (err, rows) {
     }
   }
 
-  // Function to get the data of a specific State
-  function getStateData(chosenTuition, chosenState, chosenCity) {
+  // Function to get the data of a specific State or City
+  function getStateData(chosenVariable, chosenState, chosenCity) {
 
     // Initialize list to store the data variables
     citySizeInState = [], // 1. Size of Marker "In-State Tuition"
@@ -52,22 +63,35 @@ Plotly.d3.csv('../output_charts/full_query.csv', function (err, rows) {
       hoverText = [], // 6. Information for presenting in the hover
       listLat = [], // 7. Longitude
       listLong = [], // 8. Latitude
-      citySize = []; // 9. Variable to be use depending the user selection
+      citySize = [], // 9. Variable to be use depending the user selection
+      expPerStudent = [], // 10. Variable to bring the expenditure per student
+      facSalary = [], // 11. Faculty Salary
+      tuRevPerStudent = []; // 12. Tuition Revenue per Student
 
     // The function Stores all the data to the lists
     function dataToList() {
-      currentSizeOut = tuitionOut[i] / scale; // 1. Placeholder for the loop - "Out-of-State Tuition" - Marker Size
-      currentSizeIn = tuitionIn[i] / scale; // 2. Placeholder for the loop - "In-State Tuition" - Marker Size
+      currentSizeOut = tuitionOut[i] / scaleTuition; //  1. Placeholder for the loop - "Out-of-State Tuition" - Marker Size
+      currentSizeIn = tuitionIn[i] / scaleTuition; //  2. Placeholder for the loop - "In-State Tuition" - Marker Size
 
       currentTuitionIn = tuitionOut[i] // 3. Placeholder for the loop - "Out-of-State Tuition" - Actual Value in $
       currentTuitionOut = tuitionIn[i] // 4. Placeholder for the loop - "In-State Tuition" - Actual Value in $
 
       currentCity = schoolCity[i]; // 5. Stores the list of the cities for the chosen State
       // 6. Information for the hoover text
-      var currentText = "<b>" + schoolName[i] + "</b>" + "<br><b> Out of State Tuition: </b>" + "$" + thousands_separators(tuitionOut[i])
-        + "<br><b> Tuition in State: </b>" + "$" + thousands_separators(tuitionIn[i]);
+      var currentText = "<b> Name: " + schoolName[i] + "</b>"
+        + "<br><b> City: </b>" + schoolCity[i]
+        + "<br><b> State: </b>" + schoolState[i]
+        + "<br><b> Geolocation: </b>(" + schoolLat[i] + "," + schoolLong[i] + ")"
+        + "<br><b> Out of State Tuition: </b>" + "$" + thousands_separators(tuitionOut[i])
+        + "<br><b> Tuition in State: </b>" + "$" + thousands_separators(tuitionIn[i])
+        + "<br><b> Expenditure per Student: </b>" + "$" + thousands_separators(expenditurePerStudent[i])
+        + "<br><b> Tuition Revenue per Student: </b>" + "$" + thousands_separators(tuitionRevenuePerStudent[i]);
       currentLat = schoolLat[i]; // 7. Placeholder for the loop - Latitude
       currentLong = schoolLong[i]; // 8. Placeholder for the loop - Longitude
+
+      currentExpen = expenditurePerStudent[i] / scaleExp; // B. 10. Placeholder for the loop - "Expendiure per Student - Marker Size
+      currentFacSal = facultySalary[i] / scaleFac; // 11. Placeholder for the loop - Faculty Salary - Marker Size
+      currentRevStu = tuitionRevenuePerStudent[i] / scaleRev // 12. Placeholder for the loop - Tuition Revenue per Student - Marker Size
 
       // Stores all the placeholder values to the lists
       citySizeOutState.push(currentSizeOut); // 1.
@@ -79,31 +103,60 @@ Plotly.d3.csv('../output_charts/full_query.csv', function (err, rows) {
       hoverText.push(currentText); // 6.
       listLat.push(currentLat); // 7. 
       listLong.push(currentLong); //8.
+      expPerStudent.push(currentExpen) // 10.
+      facSalary.push(currentFacSal) // 11.
+      tuRevPerStudent.push(currentRevStu) // 12.
     }
 
     // 
     for (var i = 0; i < schoolState.length; i++) { // Loops all the dataset
-      if (schoolState[i] === chosenState) { // Extracts information only for the state chosen by the user
-        dataToList() // Stores the data that fullfill the condition of chosenState
-      }
-      if (chosenState === "All") {
+      if (chosenState === "All") { // if Chosen State is "All" bring that information and
         dataToList() // Stores the data that fullfill the condition of all States
+        listofCities = [] // if Chosen State All, clear the list of cities
+      } else if (schoolState[i] === chosenState) { // Extracts information only for the state chosen by the user
+        if (chosenCity == "All") { // if it user choose all the cities, bring information of all cities
+          dataToList()
+        } else if (schoolCity[i] === chosenCity) {  // If user choose only one city, bring information from that city
+          dataToList() // Stores the data that fullfill the condition of chosenState
+        }
       }
     }
-    // Assigns the data depending on the user selection: In State Tuition or Out of State Tuition
-    if (chosenTuition === "In State") { citySize = citySizeInState }
-    else if (chosenTuition === "Out of State") { citySize = citySizeOutState }
 
+    switch (chosenVariable) {
+      case "Tuition in State":
+        citySize = citySizeInState;
+        document.querySelector('.textmap').innerText = "In-state tuition and fees"
+        scalevar = scaleTuition
+        break;
+      case "Tuition Out of State":
+        citySize = citySizeOutState;
+        document.querySelector('.textmap').innerText = "Out-of-state tuition and fees"
+        scalevar = scaleTuition
+        break;
+      case "Expenditure per Student":
+        citySize = expPerStudent;
+        document.querySelector('.textmap').innerText = "Instructional expenditures divided by the number of FTE students (undergraduates and graduate students) (http://nces.ed.gov/ipeds/glossary/index.asp?id=854). Instructional expenditures are included in the IPEDS Finance component and FTE enrollment is included in the IPEDS 12-Month Enrollment component. This metric includes graduate students."
+        scalevar = scaleExp
+        break;
+      case "Faculty Salary":
+        citySize = facSalary;
+        document.querySelector('.textmap').innerText = "Average faculty salary per month, calculated from the IPEDS Human Resources component. This metric is calculated as the total salary outlays divided by the number of months worked for all full-time nonmedical instructional staff. Prior to the 2011-12 academic year, when months worked were reported in groups, the value for 9-10 months is estimated as 9.5 months and the value for 11-12 months is estimated as 11.5 months. Values prior to the 2003-04 academic year are limited to degree-granting institutions for consistency with values in subsequent academic years."
+        scalevar = scaleFac
+        break;
+      case "Tuition Revenue per Student":
+        document.querySelector('.textmap').innerText = "Net tuition revenue (tuition revenue minus discounts and allowances) divided by the number of FTE students (undergraduates and graduate students) (http://nces.ed.gov/ipeds/glossary/index.asp?id=854). Net tuition revenue is included in the IPEDS Finance component and FTE enrollment is included in the IPEDS 12-Month Enrollment component. This metric includes graduate students."
+        citySize = tuRevPerStudent;
+        scalevar = scaleRev
+        break;
+    };
   };
-
   // Initial Values for the user selection with the dropdown
-  setBubblePlot("In State", 'All', "All")
+  setBubblePlot("Expenditure per Student", 'All', "All")
 
   // Function that creates the plots
-  function setBubblePlot(chosenTuition, chosenState, chosenCity) {
+  function setBubblePlot(chosenVariable, chosenState, chosenCity) {
     // Calls the function that brings the data depending on the user selection
-    getStateData(chosenTuition, chosenState, chosenCity);
-
+    getStateData(chosenVariable, chosenState, chosenCity);
     // Plotly setup for the map
     var data_map = [{
       type: 'scattergeo',
@@ -123,11 +176,14 @@ Plotly.d3.csv('../output_charts/full_query.csv', function (err, rows) {
 
     // Layout for the map
     var layout_map = {
-      title: 'Tuition Out of State',
+      title: chosenVariable,
+      font: {
+        family: 'Droid Serif, serif',
+        size: 24
+      },
       showlegend: false,
       geo: {
         scope: 'usa',
-        // scope: 'north america',
         resolution: 50,
         lonaxis: {
           'range': [-180, -55]
@@ -148,26 +204,75 @@ Plotly.d3.csv('../output_charts/full_query.csv', function (err, rows) {
     };
 
     // Plot Map
-    Plotly.newPlot('plotdivmap', data_map, layout_map, { showLink: false, showSendToCloud: true })
+    Plotly.newPlot('plotdivmap', data_map, layout_map, { responsive: true })
 
     // Plotly setup for the Gauge
+
+    function averageIfValue(array) {
+      var sum = 0,
+        count = 0;
+      for (var i = 0; i < array.length; i++) {
+        if (array[i] !== null && array[i] !== '') {
+          sum += parseFloat(array[i]);
+          count += 1
+        }
+      }
+      return sum / (count)
+    }
+
     var data_gauge = [
       {
         type: "indicator",
-        mode: "number+gauge+delta",
-        gauge: { shape: "bullet" },
-        delta: { reference: 300 },
-        value: 220,
-        domain: { x: [0, 1], y: [0, 1] },
-        title: { text: "Profit" }
+        mode: "gauge+number",
+        gauge: {
+          shape: "bullet", axis: { range: [null, 20000] }, 
+          steps: [
+            { range: [0, 10000], color: "white" },
+            { range: [10000, 20000], color: "lightgray" }
+          ],
+          threshold: {
+            line: { color: "red", width: 4 },
+            thickness: 0.75,
+            value: averageIfValue(citySize) * scalevar
+          }
+        },
+        value: averageIfValue(citySize) * scalevar,
+        domain: { x: [0, 1], y: [0, 0.5] },
+        title: {
+          text: "State: " + chosenState + "<br>" + "City: " + chosenCity,
+          font: {
+            family: 'Droid Serif, serif',
+            size: 15
+          }
+        },
       }
     ];
 
     // Layout for the map
-    var layout_gauge = { width: 600, height: 250 };
+    // var layout_gauge = // { width: 700, height: 220 };
+
+    var layout_gauge = {
+      title: chosenVariable + "<br>" + "(Average)",
+      autosize: false,
+      width: 700,
+      height: 100,
+      margin: {
+        l: 175,
+        r: 0,
+        b: 20,
+        t: 50,
+        pad: 5
+      },
+      yanchor: 'middle',
+      xanchor: 'middle'
+
+    }
+
+
+
 
     // Plot Gauge
-    Plotly.newPlot('plotdivgauge', data_gauge, layout_gauge);
+    Plotly.newPlot('plotdivgauge', data_gauge, layout_gauge, { responsive: true });
 
     // Plotly setup for the Table
     var trace1_table = {
@@ -249,15 +354,27 @@ Plotly.d3.csv('../output_charts/full_query.csv', function (err, rows) {
 
     // Plot Table
     Plotly.newPlot('plotdivtable', data, layout);
+
   };
 
   // Select html dropdown containing the information
-  var tuitionSelector = document.querySelector('.tuition'),
+  var variableInfoSelector = document.querySelector('.variabledata'),
     stateSelector = document.querySelector('.statedata'),
     citySelector = document.querySelector('.citydata');
 
   // Funciton that fills the a dropdown  
-  function assignOptions(textArray, selector) {
+  function assignOptions(textArray, selector, addAll, unique) {
+    // Clear previous dropdown contents
+    // selector.innerHTML = ""
+    // selector.options.length = 0
+    selector.innerText = null
+    // Sort the content in the list
+    textArray.sort()
+    // Adds all at the beginning
+    if (addAll == true) { textArray.unshift("All") }
+    // Brings only unique elements
+    if (unique == true) { textArray = (unique = [...new Set(textArray)]) }
+    // Creates the element in the dropdown in the html page
     for (var i = 0; i < textArray.length; i++) {
       var currentOption = document.createElement('option');
       currentOption.text = textArray[i];
@@ -265,30 +382,28 @@ Plotly.d3.csv('../output_charts/full_query.csv', function (err, rows) {
     }
   }
 
-  // Bring the list of the states created at the beginning of the code
-  listofStates = listofStates.sort()
-  listofStates.unshift("All")
-
-
-  listofCities = listofCities.sort(),
-    listofCities.unshift("All"),
-    listofCities = (unique = [...new Set(listofCities)]),
-
-
-
-    assignOptions(listofStates, stateSelector);
-  assignOptions(listofCities, citySelector);
-  assignOptions(["In State", "Out of State"], tuitionSelector);
+  assignOptions(listofStates, stateSelector, true, true),
+    assignOptions([], citySelector, true, true),
+    assignOptions([
+      "Tuition in State",
+      "Tuition Out of State",
+      "Expenditure per Student",
+      "Faculty Salary",
+      "Tuition Revenue per Student"
+    ], variableInfoSelector, false, true);
 
   function updateState() {
-    setBubblePlot(tuitionSelector.value, stateSelector.value, citySelector.value);
+    setBubblePlot(variableInfoSelector.value, stateSelector.value, citySelector.value)
+    assignOptions(listofCities, citySelector, true, true)
+  };
+
+  function updateCity() {
+    setBubblePlot(variableInfoSelector.value, stateSelector.value, citySelector.value)
   }
 
-  stateSelector.addEventListener('change', updateState, false);
-  citySelector.addEventListener('change', function () {
-    updateState()
-    getStateData(tuitionSelector.value, stateSelector.value, citySelector.value);
-  }, false);
-  tuitionSelector.addEventListener('change', updateState, false);
+  variableInfoSelector.addEventListener('change', updateState, false),
+    stateSelector.addEventListener('change', updateState, false),
+    citySelector.addEventListener('change', updateCity, false);
+
 
 });
